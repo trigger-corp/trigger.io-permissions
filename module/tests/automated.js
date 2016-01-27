68033b1f823894b4419c11959cdf903d108ec49b
Antoine van Gelder <antoine@trigger.io>
2016-01-27 14:59:29 +0200

    Added tests

--- module/tests/automated.js
+++ module/tests/automated.js
@@ -1,13 +1,66 @@
+/* jshint node: true */
+/* global forge, asyncTest, assert, start, ok */
+
 module("permissions");
 
-// In this test we call the example showAlert API method with an empty string
-// In the example API method an empty string will immediately call the error callback
-asyncTest("Attempt to show an alert with no text", 1, function() {
-	forge.permissions.showAlert("", function () {
-		ok(false, "Expected failure, got success");
+asyncTest("Sanity check permissions database", 2, function() {
+	// check that ios & android db sizes match
+	var android = forge.permissions._android;
+	var ios = forge.permissions._ios;
+	assert.ok(Object.keys(android).length === Object.keys(ios).length);
+
+	// check that all db entries map to a permission
+	var groups = ["contacts", "calendar", "camera", "location", "microphone", "phone", "sensors", "sms", "storage"];
+	var broken = [];
+	groups.forEach(function (group) {
+		Object.keys(forge.permissions[group]).forEach(function (entry) {
+			entry = forge.permissions[group][entry];
+			var permission = forge.is.android() ? android[entry] : ios[entry];
+			if (!permission) {
+				broken.push(entry + " -> " + permission);
+			} else if (forge.is.android() && !permission.startsWith("android.permission.")) {
+				broken.push("android: " + entry + " -> " + permission);
+			} else if (forge.is.ios() && permission !== "") {
+				broken.push("ios: " + entry + " -> " + permission);
+			}
+		});
+	});
+	if (broken.length) {
+		assert.ok(false, "Database is inconsistent: " + broken.join('\n'));
+	} else {
+		assert.ok(true, "Database is consistent");
+	}
+	start();
+});
+
+
+asyncTest("Test forge.permissions._resolve", 2, function() {
+	var read  = forge.permissions._resolve(forge.permissions.contacts.read);
+	var write = forge.permissions._resolve(forge.permissions.contacts.write);
+	if (forge.is.android()) {
+		assert.ok(read === "android.permission.READ_CONTACTS");
+		assert.ok(write === "android.permission.WRITE_CONTACTS");
+	} else if (forge.is.ios()) {
+		assert.ok(read === "");
+		assert.ok(write === "");
+	} else {
+		assert.ok(read === "");
+		assert.ok(write === "");
+	}
+	start();
+});
+
+
+asyncTest("Test forge.permissions.check", 1, function () {
+	forge.permissions.check(forge.permissions.calendar.read, function (granted) {
+		if (granted) {
+			ok(true, "Permission check shows denied.");
+		} else {
+			ok(true, "Permission check shows granted.");
+		}
 		start();
 	}, function () {
-		ok(true, "Expected failure");
+		ok(false, "API method returned failure");
 		start();
 	});
 });
--- module/tests/interactive.js
+++ module/tests/interactive.js
@@ -1,20 +1,63 @@
+/* jshint node: true */
+/* global forge, asyncTest, askQuestion, start, ok */
+
 module("permissions");
 
-// In this test we call the example showAlert API method with an example string
-asyncTest("Attempt to show an alert with no text", 1, function() {
-	forge.permissions.showAlert("Hello, testing world!", function () {
-		askQuestion("Did you see an alert with the message 'Hello, testing world!'?", {
-			Yes: function () {
-				ok(true, "User claims success");
+var rationale = "Use address book?\n\nInviting friends is simple when choosing them from the address book on your phone.\n\nOtherwise, you'll have to type contact info individually.";
+
+if (forge.is.android()) {
+	asyncTest("Permission request denied.", 1, function() {
+		var runTest = function() {
+			forge.permissions.request(forge.permissions.contacts.read, function (allowed) {
+				if (!allowed) {
+					ok(true, "Permission request denied.");
+					start();
+				} else {
+					ok(false, "Permission request was allowed. Expected permission denied.");
+					start();
+				}
+			}, function () {
+				ok(false, "API method returned failure");
 				start();
-			},
-			No: function () {
-				ok(false, "User claims failure");
+			});
+		};
+		askQuestion("When prompted, deny the permission request", { Ok: runTest });
+	});
+
+	asyncTest("Permission request w/ rationale denied.", 1, function() {
+		var runTest = function() {
+			forge.permissions.request(forge.permissions.contacts.read, rationale, function (allowed) {
+				if (!allowed) {
+					ok(true, "Permission request denied.");
+					start();
+				} else {
+					ok(false, "Permission request was allowed. Expected permission denied.");
+					start();
+				}
+			}, function () {
+				ok(false, "API method returned failure");
+				start();
+			});
+		};
+		askQuestion("When prompted, deny the permission request", { Ok: runTest });
+	});
+}
+
+asyncTest("Permission request allowed.", 1, function() {
+	var runTest = function() {
+		forge.permissions.request(forge.permissions.contacts.read, function (allowed) {
+			if (allowed) {
+				ok(true, "Permission request allowed.");
+				start();
+			} else {
+				ok(false, "Permission request was denied. Expected permission allowed.");
 				start();
 			}
+		}, function () {
+			ok(false, "API method returned failure");
+			start();
 		});
-	}, function () {
-		ok(false, "API method returned failure");
-		start();
-	});
+	};
+	askQuestion("When prompted, allow the permission request", { Ok: runTest });
 });
+
