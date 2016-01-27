3aacb1ca7be797a4eea0f899f6bfea4120ea838e
Antoine van Gelder <antoine@trigger.io>
2016-01-27 15:03:15 +0200

    Android API implementation

--- inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/permissions/API.java
+++ inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/permissions/API.java
@@ -1,28 +1,83 @@
 package io.trigger.forge.android.modules.permissions;
 
+import io.trigger.forge.android.core.ForgeActivity.EventAccessBlock;
 import io.trigger.forge.android.core.ForgeApp;
+import io.trigger.forge.android.core.ForgeLog;
 import io.trigger.forge.android.core.ForgeParam;
 import io.trigger.forge.android.core.ForgeTask;
+
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
 public class API {
-	public static void showAlert(final ForgeTask task, @ForgeParam("text") final String text) {
-		if (text.length() == 0) {
-			// Error if there is no text to show
-			task.error("No text entered");
+
+	public static void check(final ForgeTask task, @ForgeParam("permission") final String permission) {
+		int ret = ContextCompat.checkSelfPermission(ForgeApp.getActivity(), permission);
+		if (ret == PackageManager.PERMISSION_GRANTED) {
+			task.success(true);
+		} else {
+			task.success(false);
+		}
+	}
+
+
+	public static void request(final ForgeTask task, @ForgeParam("permission") final String permission) {
+		// Check if the permission has already been granted
+		int ret = ContextCompat.checkSelfPermission(ForgeApp.getActivity(), permission);
+		if (ret == PackageManager.PERMISSION_GRANTED) {
+			task.success(true);
 			return;
 		}
+
+		// Get Rationale for permission if specified
+		final String rationale;
+		if (task.params.has("rationale")) {
+			rationale = task.params.get("rationale").getAsString();
+		} else {
+			rationale = null;
+		}
+
+		final EventAccessBlock eventAccessBlock = new EventAccessBlock() {
+			@Override
+			public void run(boolean granted) {
+				task.success(granted);
+			}
+		};
+
+		// shouldShowRequestPermissionRationale returns:
+		// first time:                        false
+		// if user clicked deny first time:   true
+		// if user clicked "Never ask again": false
+		if (rationale != null && ActivityCompat.shouldShowRequestPermissionRationale(ForgeApp.getActivity(), permission)) {
+			showRationale(task, rationale, new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int selection) {
+					ForgeLog.d("Clicked: " + selection);
+					if (selection == -2) { // no
+						task.success(false);
+					} else { // yes
+						ForgeApp.getActivity().requestPermission(permission, eventAccessBlock);
+					}
+				}
+			});
+		} else {
+			ForgeApp.getActivity().requestPermission(permission, eventAccessBlock);
+		}
+	}
+
+
+	protected static void showRationale(final ForgeTask task, final String message, final DialogInterface.OnClickListener listener) {
 		task.performUI(new Runnable() {
 			public void run() {
-				AlertDialog.Builder builder = new AlertDialog.Builder(ForgeApp.getActivity());
-				builder.setMessage(text).setCancelable(false).setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int which) {
-						task.success();
-					}
-				});
-				AlertDialog alert = builder.create();
-				alert.show();
+				new AlertDialog.Builder(ForgeApp.getActivity())
+						.setMessage(message)
+						.setPositiveButton("Okay", listener)
+						.setNegativeButton("No Thanks", listener)
+						.create()
+						.show();
 			}
 		});
 	}
--- inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/permissions/EventListener.java
+++ inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/permissions/EventListener.java
@@ -1,11 +1,13 @@
 package io.trigger.forge.android.modules.permissions;
 
-import io.trigger.forge.android.core.ForgeApp;
 import io.trigger.forge.android.core.ForgeEventListener;
+//import io.trigger.forge.android.core.ForgeLog;
 
 public class EventListener extends ForgeEventListener {
-	@Override
-	public void onRestart() {
-		ForgeApp.event("permissions.resume", null);
-	}
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        //ForgeLog.d("permissions.onRequestPermissionsResult -> " + requestCode + " -> " + permissions + " -> " + grantResults);
+    }
 }
+
