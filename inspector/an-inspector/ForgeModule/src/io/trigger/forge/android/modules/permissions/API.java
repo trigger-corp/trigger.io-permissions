eef8a79a39f7f3fdefafd740d95c7a1ddf25db00
Antoine van Gelder <antoine@trigger.io>
2019-08-16 13:29:55 +0200

    Migrate to AndroidX

--- inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/permissions/API.java
+++ inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/permissions/API.java
@@ -9,8 +9,8 @@ import io.trigger.forge.android.core.ForgeTask;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 
 public class API {
